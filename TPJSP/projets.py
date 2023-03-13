4f13a0cdacdff01eb4ce9d1f1fd4528931b178b9
Tutanka01 <40025011+Tutanka01@users.noreply.github.com>
2023-03-13 12:39:12 +0000

    m

--- TPJSP/projets.py
+++ TPJSP/projets.py
@@ -1,26 +1,22 @@
 import sqlite3 as sql
 con = sql.connect("bdd2.sqlite")
 cur = con.cursor()
-res = [x for x in cur.execute("SELECT nometud, COUNT(*) FROM etudiant, projet WHERE etudiant.idetud = projet.idetud GROUP BY nometud HAVING COUNT(*) > 0")]
-print(res[0][1])
+res = [x for x in cur.execute("SELECT nometud, COUNT(*) FROM etudiant, projet WHERE etudiant.idetud = projet.idetud GROUP BY nometud HAVING COUNT(*)")]
 
-def html_table(res):
-    html = "<HTML><body><table style='border: 1px solid black;'>"
-    for i in range(0, len(res)):
-        html += "<tr>"
-        if res[i][1] == 1:
-            html += "<td>{}</td>".format(res[i][0])
-            html += "<td style='border: 1px solid black; background-color:yellow;'>{}</td>".format(res[i][1])
-        elif res[i][1] == 0:
-            html += "<td>{}</td>".format(res[i][0])
-            html += "<td style='border: 1px solid black; background-color:red;'>{}</td>".format(res[i][1])
-        elif res[i][1] > 1:
-            html += "<td>{}</td>".format(res[i][0])
-            html += "<td style='border: 1px solid black; background-color:green;'>{}</td>".format(res[i][1])
-        else:
-            html += "<td style='border: 1px solid black;'>{}</td>".format(res[i][1])
-        html += "</tr>"
-    html += "</table></body></HTML>"
-    return html
-
-print(html_table(res))
+html = "<HTML><body><table style='border: 1px solid black;'>"
+for i in range(0, len(res)):
+    html += "<tr>"
+    if res[i][1] == 1:
+        html += "<td>{}</td>".format(res[i][0])
+        html += "<td style='border: 1px solid black; background-color:yellow;'>{}</td>".format(res[i][1])
+    elif res[i][1] == 0:
+        html += "<td>{}</td>".format(res[i][0])
+        html += "<td style='border: 1px solid black; background-color:red;'>{}</td>".format(res[i][1])
+    elif res[i][1] > 1:
+        html += "<td>{}</td>".format(res[i][0])
+        html += "<td style='border: 1px solid black; background-color:green;'>{}</td>".format(res[i][1])
+    else:
+        html += "<td style='border: 1px solid black;'>{}</td>".format(res[i][1])
+    html += "</tr>"
+html += "</table></body></HTML>"
+print(html)
